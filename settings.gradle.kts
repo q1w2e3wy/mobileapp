86b103e86a5c44c9a50c6da7f38ffc8a18893e72
q1w2e3wy <syw13056@gmail.com>
2025-10-28 10:45:48 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+include(":week3")
+include(":week6")
+include(":week4")
+include(":week5")
---
+++ week4/src/main/java/com/example/week4/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.week4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.week4.ui.theme.MyApplicationTheme
+
+
+
+
+private val Unit.body: Any
+
+private val Unit.autor: Any
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Text(
+        text = "Compose Modifier를 배워보자!",
+        modifier = Modifier
+            .padding(start = 16.dp,
+                top = 16.dp,)
+            .background(Color.Yellow)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    HomeScreen()
+}
